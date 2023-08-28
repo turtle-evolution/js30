76066b96ae5fc99249d008822c8701d68ec47ad8
Huong Tram Thi Thuy <huong.tram@stashnextgen.com>
2023-08-28 12:33:32 +0700

    day27

---
+++ day27/main.js
@@ -0,0 +1,40 @@
+const slider = document.querySelector('.items');
+let isDown = false;
+let startX;
+let scrollLeft;
+const inputNumber = document.querySelector('input');
+
+function itemSlider(e) {
+
+    const number = inputNumber.value || 1;
+
+    if(!isDown) return; //stop the fn from running
+    e.preventDefault();
+
+    const x = e.pageX - slider.offsetLeft;
+
+    const walk = (x - startX) * number;
+
+    slider.scrollLeft = scrollLeft - walk;
+}
+
+slider.addEventListener('mousedown', (e) => {
+    isDown = true;
+    slider.classList.add('active');
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+
+    console.log(e.pageX, scrollLeft);
+})
+
+slider.addEventListener('mouseup', () => {
+    isDown = false;
+    slider.classList.remove('active');
+})
+
+slider.addEventListener('mouseleave', () => {
+    isDown = false;
+    slider.classList.remove('active');
+})
+
+slider.addEventListener('mousemove', itemSlider);
